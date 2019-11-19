a429014e6966b834c50009d051dcaeec9e183546
Afolabi Badmos <afolabi@autonomic.ai>
2019-11-19 09:16:49 -0500

    Build other associated images in build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,30 @@
-make controller
+BINS=(
+  "controller"
+  "webhook"
+  "cainjector"
+)
 
-mv ./bazel-bin/cmd/controller/linux_amd64_pure_stripped/controller .
+for bin in "${BINS[@]}"; do
+  make $bin
 
-cat <<EOF > Dockerfile
+  mv ./bazel-bin/cmd/${bin}/linux_amd64_pure_stripped/${bin} .
+
+  cat <<EOF > Dockerfile.${bin}
 FROM alpine:3.10.3
 
 RUN apk add --no-cache ca-certificates && \
 addgroup -S certmanager && adduser -S -G certmanager certmanager
 
-ADD controller /usr/bin/cert-manager
+ADD $bin /usr/bin/${bin}
 
 USER certmanager
-ENTRYPOINT ["/usr/bin/cert-manager"]
+ENTRYPOINT [ "/usr/bin/${bin}" ]
 ARG VCS_REF
 LABEL org.label-schema.vcs-ref=$VCS_REF \
   org.label-schema.vcs-url="https://github.com/jetstack/cert-manager" \
   org.label-schema.license="Apache-2.0"
 EOF
+done
 
 cat <<EOF > build_args
 {"VCS_REF": "$(git rev-parse HEAD)"}
